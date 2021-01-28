9946ca741a8b93276ca5fbbe023ac971fd672f78
Ryush <seunghyun87.ryu@gmail.com>
2021-01-28 17:53:59 +0900

    - 마이페이지 > 내문의내역 팝업 텍스트 정렬 수정 - 공유하기 팝업 url 영역 추가 작업

--- common/js/sem-vue/components/conner/popup-share.vue
+++ common/js/sem-vue/components/conner/popup-share.vue
@@ -5,6 +5,7 @@
       <div class="inner-box">
         <strong class="modal_tit">공유하기</strong>
         <div class="modal_cont">
+
           <div class="share-box">
             <div class="share-kakao-btn">
               <a href="#none" class="btn-share" data-param-url="">카카오톡 공유</a>
@@ -13,6 +14,14 @@
               <a href="#none" class="btn-share" @click="copyUrlForShare">URL</a>
             </div>
           </div>
+
+          <!-- mkp-add : 20210128 -->
+          <div class="ipt-txt-box">
+            <input type="text" class="input-type-rn" readonly value="https://bit.ly/20dkfh3kshdd20dkfh3kshdd20dkfh3kshdd20dkfh3kshdd20dkfh3kshdd20dkfh3kshdd20dkfh3kshdd">
+            <button type="button" class="url-copy-btn" @click="copyUrlForShare"><span>복사</span></button>
+          </div>
+          <!-- //mkp-add : 20210128 -->
+
         </div>
         <button type="button" class="icon-header-close">팝업닫기</button>
       </div>
